67fcab4f11c1175065225f1e4fcd8ff1b02e5308
Lucas <fazendalbarbosa>
2019-10-25 14:41:14 -0400

    atualizando get

--- mian.go
+++ mian.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"github.com/gorilla/mux"
+	"github.com/lucasdasilvabarbosa/rest-api/models"
 	"log"
 	"net/http"
 )
 
+var people []models.Person
+
 func main() {
+	people = append(people, models.Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &models.Address{City: "City X", State: "State X"}})
 	router := mux.NewRouter()
 	router.HandleFunc("/contato", GetPeople).Methods("GET")
 	router.HandleFunc("/contato/{id}", GetPerson).Methods("GET")
@@ -14,12 +19,13 @@ func main() {
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-//var people []Person
-//
-//people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
-
-func CreatPerson(w http.ResponseWriter, request *http.Request) {
-
+func CreatPerson(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var person models.Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
 func GetPerson(w http.ResponseWriter, request *http.Request) {
@@ -28,4 +34,5 @@ func GetPerson(w http.ResponseWriter, request *http.Request) {
 
 func GetPeople(w http.ResponseWriter, r *http.Request) {
 	print("chegou")
+	json.NewEncoder(w).Encode(people)
 }
